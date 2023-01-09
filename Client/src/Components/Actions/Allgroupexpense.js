250cb7765d21e14755934d7f86835dbe9b82b748
Kartik Gupta <109219148+CartikKG@users.noreply.github.com>
2023-01-09 19:35:05 +0530

    groupExpense

---
+++ Client/src/Components/Actions/Allgroupexpense.js
@@ -0,0 +1,18 @@
+export const createNewgroup=()=>{
+
+}
+export const addDatatogroup=()=>{
+
+}
+export const editDatafromgroup=()=>{
+
+}
+export const deleteDatafromgroup=()=>{
+
+}
+export const inviteUsertogroup=()=>{
+
+}
+export const switchgroup=()=>{
+
+}
